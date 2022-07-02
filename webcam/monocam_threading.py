c9ef01a562b42452c672ccc0c4989def7f6d6764
ZainUlMustafa <zain2984@gmail.com>
2022-07-02 20:32:20 +0500

    monocam threading

---
+++ webcam/monocam_threading.py
@@ -0,0 +1,39 @@
+# import required libraries
+from vidgear.gears import VideoGear
+import cv2
+import time
+
+# define and start the stream on first source ( For e.g #0 index device)
+stream1 = VideoGear(source=0, logging=True).start()
+
+# infinite loop
+while True:
+    
+    frameA = stream1.read()
+    # read frames from stream1
+
+    # check if any of two frame is None
+    if frameA is None:
+        #if True break the infinite loop
+        break
+    
+    # do something with both frameA and frameB here
+    cv2.imshow("Output Frame1", frameA)
+    # Show output window of stream1 and stream 2 seperately
+
+    key = cv2.waitKey(1) & 0xFF
+    # check for 'q' key-press
+    if key == ord("q"):
+        #if 'q' key-pressed break out
+        break
+
+    if key == ord("w"):
+        #if 'w' key-pressed save both frameA and frameB at same time
+        cv2.imwrite("Image-1.jpg", frameA)
+        #break   #uncomment this line to break out after taking images
+
+cv2.destroyAllWindows()
+# close output window
+
+# safely close both video streams
+stream1.stop()
