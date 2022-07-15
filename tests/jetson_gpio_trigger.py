94c6e828a374604a54fa7dc5a0edaf54da819f72
straxi <zain2984@gmail.com>
2022-07-15 17:58:03 +0500

    jetson gpio

---
+++ tests/jetson_gpio_trigger.py
@@ -0,0 +1,22 @@
+import Jetson.GPIO as GPIO
+
+# Handles time
+import time 
+ 
+# Pin Definition
+led_pin = 7
+ 
+# Set up the GPIO channel
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.HIGH)
+
+ 
+# Blink the LED
+while True: 
+    time.sleep(0.0499) 
+    GPIO.output(led_pin, GPIO.HIGH) 
+    print("LED is ON")
+    time.sleep(0.15) 
+    GPIO.output(led_pin, GPIO.LOW)
+    print("LED is OFF")
+#endwhile
