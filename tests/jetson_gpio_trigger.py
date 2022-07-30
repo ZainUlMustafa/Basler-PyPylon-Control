3885d6c981683f9db2d02b4c9412bf55dd21f7f6
straxi <zain2984@gmail.com>
2022-07-30 09:30:58 +0500

    buzzer trigger test

--- tests/jetson_gpio_trigger.py
+++ tests/jetson_gpio_trigger.py
@@ -4,19 +4,61 @@ import Jetson.GPIO as GPIO
 import time 
  
 # Pin Definition
-led_pin = 7
+led_pin = 11
  
 # Set up the GPIO channel
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.HIGH)
 
  
-# Blink the LED
-while True: 
-    time.sleep(0.5) 
-    GPIO.output(led_pin, GPIO.HIGH) 
-    print("LED is ON")
-    time.sleep(0.5) 
-    GPIO.output(led_pin, GPIO.LOW)
-    print("LED is OFF")
-#endwhile
+# # Blink the LED
+# while True: 
+#     time.sleep(0.5) 
+#     GPIO.output(led_pin, GPIO.HIGH) 
+#     print("LED is ON")
+#     time.sleep(0.5) 
+#     GPIO.output(led_pin, GPIO.LOW)
+#     print("LED is OFF")
+# #endwhile
+
+# BEEP BEEP BEEP BEEEEEEP
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.1)
+GPIO.output(led_pin, GPIO.LOW)
+time.sleep(0.05)
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.1)
+GPIO.output(led_pin, GPIO.LOW)
+time.sleep(0.05)
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.1)
+GPIO.output(led_pin, GPIO.LOW)
+time.sleep(0.05)
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.5)
+GPIO.output(led_pin, GPIO.LOW)
+
+time.sleep(1)
+
+# BEEEEEP BEEP BEEP
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.5)
+GPIO.output(led_pin, GPIO.LOW)
+time.sleep(0.05)
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.1)
+GPIO.output(led_pin, GPIO.LOW)
+time.sleep(0.05)
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.1)
+GPIO.output(led_pin, GPIO.LOW)
+
+time.sleep(1)
+
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.09)
+GPIO.output(led_pin, GPIO.LOW)
+time.sleep(0.05)
+GPIO.output(led_pin, GPIO.HIGH) 
+time.sleep(0.13)
+GPIO.output(led_pin, GPIO.LOW)
